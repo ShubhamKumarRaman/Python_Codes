755fc37d262c02caecc8fa8efeb7a5584269925c
ShubhamKumarRaman <shubham620409@gmail.com>
2025-03-27 19:27:31 +0530

    Update 03Factorial.py

--- 01BasicPrograms/03Factorial.py
+++ 01BasicPrograms/03Factorial.py
@@ -3,3 +3,10 @@ fact = 1
 for i in range(1,n+1):
     fact *= i
 print(f"Factorial is : {fact}")
+
+# using recursion
+def factorial (n):
+    return 1 if(n==1 or n==0) else n*(factorial(n-1))
+
+fact = factorial(6)
+print(f"Factorial is : {fact}")
