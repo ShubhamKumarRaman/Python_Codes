4dfa1bd309977f931fe13d744817a36d246c6243
ShubhamKumarRaman <shubham620409@gmail.com>
2025-04-11 23:36:49 +0530

    Update 04SimpleInterest.py

--- 01BasicPrograms/04SimpleInterest.py
+++ 01BasicPrograms/04SimpleInterest.py
@@ -9,3 +9,12 @@ p, t, r = 8, 6, 8
 res = fun(p, t, r)
 
 print(res)
+
+# lambda function
+si = lambda p, t, r: (p * t * r) / 100
+
+# given values for principal (p), time (t) in years, and rate of interest (r) per annum
+p, t, r = 8, 6, 8
+
+res = si(p, t, r)
+print(res)
