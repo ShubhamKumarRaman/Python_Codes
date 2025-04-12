05f794fdf4e795e5563524ddf9dfb860d74ffa87
ShubhamKumarRaman <shubham620409@gmail.com>
2025-04-12 17:39:00 +0530

    Update 05CompoundInterest.py

--- 01BasicPrograms/05CompoundInterest.py
+++ 01BasicPrograms/05CompoundInterest.py
@@ -34,3 +34,16 @@ time = int(input("Enter time in years: " ))
 compound_interest(principal,rate,time)
 
 #This code is contributed by Vinay Pinjala.
+
+# Python code
+# To find compound interest 
+
+# inputs 
+p= 1200 # principal amount 
+t= 2	 # time 
+r= 5.4 # rate 
+# calculates the compound interest
+a=p*(1+(r/100))**t # formula for calculating amount 
+ci=a-p # compound interest = amount - principal amount
+# printing compound interest value
+print(ci)
