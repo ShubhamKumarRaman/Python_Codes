d8894e1aa20727f83a315e26b2ff61b1c04af405
ShubhamKumarRaman <shubham620409@gmail.com>
2025-04-15 23:22:34 +0530

    Update 08PrimeNumber.py

--- 01BasicPrograms/08PrimeNumber.py
+++ 01BasicPrograms/08PrimeNumber.py
@@ -15,3 +15,24 @@ for i in range(2, int(y ** 0.5) + 1):
 # collect primes in the range [x, y]
 res = [i for i in range(x, y + 1) if primes[i]]
 print(res if res else "No")
+
+
+
+x, y = 2, 7  # define range [x, y]
+
+res = []  # list to store primes
+
+for n in range(x, y + 1):
+    if n <= 1:
+        continue  # skip non-primes <= 1
+
+    is_prime = True  # assume n is prime
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            is_prime = False  # n is not prime
+            break
+
+    if is_prime:
+        res.append(n)  # add prime number
+
+print(res if res else "No")
