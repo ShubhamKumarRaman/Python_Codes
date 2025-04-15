169d23125475153b51bb03090c6d9c254860bcb9
ShubhamKumarRaman <shubham620409@gmail.com>
2025-04-15 23:23:03 +0530

    Update 08PrimeNumber.py

--- 01BasicPrograms/08PrimeNumber.py
+++ 01BasicPrograms/08PrimeNumber.py
@@ -43,3 +43,18 @@ x, y = 2, 7  # range [x, y]
 
 primes = list(primerange(x, y + 1))
 print(primes if primes else "No")
+
+
+x, y = 2, 7  # range [x, y]
+res = []  # list to store primes
+
+for i in range(x, y + 1):
+    if i <= 1:
+        continue  # skip non-primes <= 1
+    for j in range(2, i // 2 + 1):
+        if i % j == 0:
+            break  # not a prime
+    else:
+        res.append(i)  # prime found
+
+print(res if res else "No")
