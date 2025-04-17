e03630c80cc6648245fe78811ad624e718cfdf54
ShubhamKumarRaman <shubham620409@gmail.com>
2025-04-17 23:29:21 +0530

    Update 10PrimeCheck.py

--- 01BasicPrograms/10PrimeCheck.py
+++ 01BasicPrograms/10PrimeCheck.py
@@ -9,3 +9,16 @@ g3 = isprime(2)
 print(g1) 
 print(g2) 
 print(g3) 
+
+import math
+
+n = 11
+if n <= 1:
+    print(False)
+else:
+    is_prime = True
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            is_prime = False
+            break
+    print(is_prime)
