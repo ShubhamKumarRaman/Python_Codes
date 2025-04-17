e3a380f4d874ba63abf5c786870f11d809ade245
ShubhamKumarRaman <shubham620409@gmail.com>
2025-04-17 23:29:35 +0530

    Update 10PrimeCheck.py

--- 01BasicPrograms/10PrimeCheck.py
+++ 01BasicPrograms/10PrimeCheck.py
@@ -22,3 +22,21 @@ else:
             is_prime = False
             break
     print(is_prime)
+
+
+from math import sqrt
+
+n = 17
+p_fl = 0
+
+if(n > 1):
+    for i in range(2, int(sqrt(n)) + 1):
+        if (n % i == 0):
+            p_fl = 1
+            break
+    if (p_fl == 0):
+        print("True")
+    else:
+        print("False")
+else:
+    print("False")
