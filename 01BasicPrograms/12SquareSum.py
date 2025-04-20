152aefb127f6a7d1b5f58ce85f592224591658a5
ShubhamKumarRaman <shubham620409@gmail.com>
2025-04-20 22:17:12 +0530

    Update 12SquareSum.py

--- 01BasicPrograms/12SquareSum.py
+++ 01BasicPrograms/12SquareSum.py
@@ -37,3 +37,11 @@ sum_of_squares = sum(squares_list)
 
 # Print the result
 print("Sum of squares of first", N, "natural numbers is", sum_of_squares)
+
+
+n = 4
+sum = 0
+for i in range(1, n+1):
+    sum += i**2
+
+print("The sum of squares of first", n, "natural numbers is", sum)
