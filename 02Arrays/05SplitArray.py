4de369afc922d9e53589aa424017bd7a780c91f6
ShubhamKumarRaman <shubham620409@gmail.com>
2025-05-08 07:51:10 +0530

    Update 05SplitArray.py

--- 02Arrays/05SplitArray.py
+++ 02Arrays/05SplitArray.py
@@ -69,16 +69,11 @@ print(*result)
 
 from collections import deque
 
-def splitArr(a, n, k):
-q = deque(a)
-q.rotate(-k)
-return list(q)
+def splitArr(a, k):
+    q = deque(a)
+    q.rotate(-k)
+    return list(q)
 
-# main
 arr = [12, 10, 5, 6, 52, 36]
-n = len(arr)
-position = 2
-arr = splitArr(arr, n, position)
-for i in range(0, n):
-print(arr[i], end=' ')
-
+arr = splitArr(arr, 2)
+print(*arr)
