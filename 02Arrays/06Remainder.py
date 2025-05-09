104f2e370fedd73a705b3da9366d5d08b351c386
ShubhamKumarRaman <shubham620409@gmail.com>
2025-05-09 22:56:41 +0530

    Update 06Remainder.py

--- 02Arrays/06Remainder.py
+++ 02Arrays/06Remainder.py
@@ -40,4 +40,19 @@ n = 11
 # all the numbers
 print( findremainder(arr, lens, n))
 
-# This code is contributed by "rishabh_jain".
+from functools import reduce
+
+def remainderAfterMultiplication(arr, n):
+	result = reduce(lambda x, y: (x * y) % n, arr)
+	return result
+
+# Driver Code
+arr1 = [100, 10, 5, 25, 35, 14]
+n1 = 11
+result1 = remainderAfterMultiplication(arr1, n1)
+print(result1)
+
+arr2 = [100, 10]
+n2 = 5
+result2 = remainderAfterMultiplication(arr2, n2)
+print(result2)
