4e000ba415a52d2964df3ff3e37d9330ae1e82d2
ShubhamKumarRaman <shubham620409@gmail.com>
2025-05-11 20:38:43 +0530

    Update 07Monotonic.py

--- 02Arrays/07Monotonic.py
+++ 02Arrays/07Monotonic.py
@@ -52,3 +52,17 @@ print(isMonotonic(arr1))  # should return True
 print(isMonotonic(arr2))  # should return True
 print(isMonotonic(arr3))  # should return True
 print(isMonotonic(arr4))  # should return False
+
+def is_monotonic(arr):
+    unique_elements = set(arr)
+    increasing = sorted(arr) == arr or sorted(arr, reverse=True) == arr
+    return increasing
+
+# Driver Code
+arr1 = [6, 5, 4, 4]
+arr2 = [5, 15, 20, 10]
+arr3 = [2, 2, 2, 3]
+
+print(is_monotonic(arr1))  
+print(is_monotonic(arr2))  
+print(is_monotonic(arr3))
