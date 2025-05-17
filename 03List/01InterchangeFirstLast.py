504b1922b70596ce47e16cbff4eb39574ecd239f
ShubhamKumarRaman <shubham620409@gmail.com>
2025-05-17 16:42:41 +0530

    Update 01InterchangeFirstLast.py

--- 03List/01InterchangeFirstLast.py
+++ 03List/01InterchangeFirstLast.py
@@ -38,3 +38,21 @@ def swapList(list):
 # Driver code
 newList = [12, 35, 9, 56, 24]
 print(swapList(newList))
+
+def swap_first_last_3(lst):
+    # Check if list has at least 2 elements
+    if len(lst) >= 2:
+        # Swap the first and last elements using slicing
+        lst = lst[-1:] + lst[1:-1] + lst[:1]
+    return lst
+
+# Initializing the input
+inp=[12, 35, 9, 56, 24]
+
+# Printing the original input
+print("The original input is:",inp)
+
+result=swap_first_last_3(inp)
+
+# Printing the result
+print("The output after swap first and last is:",result)
