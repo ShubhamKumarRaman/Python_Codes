3b77a3a26a6d3932f2dfca1257e50d0cd8702061
ShubhamKumarRaman <shubham620409@gmail.com>
2025-05-19 12:00:00 +0000

    Commit on May 19, 2025

--- 03List/04CheckElement.py
+++ 03List/04CheckElement.py
@@ -5,3 +5,20 @@ if 30 in a:
     print("Element exists in the list")
 else:
     print("Element does not exist")
+
+
+a = [10, 20, 30, 40, 50]
+
+# Check if 30 exists in the list using a loop
+key = 30
+flag = False
+
+for val in a:
+    if val == key:
+        flag = True
+        break
+
+if flag:
+    print("Element exists in the list")
+else:
+    print("Element does not exist")
