fae87f3a3f3bf53f4c09d447de87093136b28e6d
ShubhamKumarRaman <shubham620409@gmail.com>
2025-05-20 00:21:48 +0530

    Update 02SwapElement.py

--- 03List/02SwapElement.py
+++ 03List/02SwapElement.py
@@ -5,3 +5,14 @@ a = [10, 20, 30, 40, 50]
 a[0], a[4] = a[4], a[0]
 
 print(a)
+
+# Using a Temporary Variable
+a = [10, 20, 30, 40, 50]
+
+# Using a temporary variable
+# to swap elements at index 2 and 4
+temp = a[2]
+a[2] = a[4]
+a[4] = temp
+
+print(a)
