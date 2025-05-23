5bd46814e37fb34301bcb639ed1fb753b18d6712
ShubhamKumarRaman <shubham620409@gmail.com>
2025-05-23 23:07:12 +0530

    Update 04CheckElement.py

--- 03List/04CheckElement.py
+++ 03List/04CheckElement.py
@@ -22,3 +22,14 @@ if flag:
     print("Element exists in the list")
 else:
     print("Element does not exist")
+
+# Using any()
+a = [10, 20, 30, 40, 50]
+
+# Check if 30 exists using any() function
+flag = any(x == 30 for x in a)
+
+if flag:
+    print("Element exists in the list")
+else:
+    print("Element does not exist")
