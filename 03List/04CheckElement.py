4495c206bea18d2110f4ba266b926bdb7cea1c88
ShubhamKumarRaman <shubham620409@gmail.com>
2025-05-23 23:07:46 +0530

    Update 04CheckElement.py

--- 03List/04CheckElement.py
+++ 03List/04CheckElement.py
@@ -33,3 +33,12 @@ if flag:
     print("Element exists in the list")
 else:
     print("Element does not exist")
+
+# Using count()
+a = [10, 20, 30, 40, 50]
+
+# Check if 30 exists in the list using count()
+if a.count(30) > 0:
+    print("Element exists in the list")
+else:
+    print("Element does not exist")
