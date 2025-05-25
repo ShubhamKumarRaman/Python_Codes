6ccacfaf9e3a633dcfabfe348895b64fafcf798c
ShubhamKumarRaman <shubham620409@gmail.com>
2025-05-25 12:00:00 +0000

    Commit on May 25, 2025

--- 03List/06ReverseList.py
+++ 03List/06ReverseList.py
@@ -3,3 +3,11 @@ a = [1, 2, 3, 4, 5]
 # Reverse the list in-place
 a.reverse()
 print(a)
+
+a = [1, 2, 3, 4, 5]
+
+# Create a new list that is a reversed list
+# of 'a' using slicing
+rev = a[::-1]
+
+print(rev)
