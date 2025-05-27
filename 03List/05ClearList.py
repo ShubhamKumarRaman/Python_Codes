66a71536114a39a2dd1d01585d6cabf41c35dccd
ShubhamKumarRaman <shubham620409@gmail.com>
2025-05-27 23:52:23 +0530

    Update 05ClearList.py

--- 03List/05ClearList.py
+++ 03List/05ClearList.py
@@ -1,3 +1,12 @@
 a = [1, 2, 3, 4, 5]
 a.clear()
 print("List after clearing:", a)
+
+# Reassigning to an Empty List
+a = [1, 2, 3, 4, 5]
+
+# Reassign 'a' to a new empty list
+a = []
+
+# Print the current state of 'a'
+print("List after reassignment:", a)
