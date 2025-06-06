8dcf0115f8c94f628a62090959990dfe95fcad9b
ShubhamKumarRaman <shubham620409@gmail.com>
2025-06-06 22:56:56 +0530

    Update 09SmallestInList.py

--- 03List/09SmallestInList.py
+++ 03List/09SmallestInList.py
@@ -3,3 +3,19 @@ a = [8, 3, 5, 1, 9, 12]
 # Find the smallest number
 smallest = min(a)
 print(smallest)
+
+a = [8, 3, 5, 1, 9, 12]
+
+# Initialize "smallest" value with first element of list
+smallest = a[0]
+
+# Iterate through list to find smallest element
+for val in a:
+  
+    # If current value is smaller than current smallest value
+    if val < smallest:
+      
+        # Update the smallest value
+        smallest = val
+
+print(smallest)
