8a47130b2bb4948193f1fdce8e791ba1238a4604
ShubhamKumarRaman <shubham620409@gmail.com>
2025-06-07 17:03:12 +0530

    Update 10LargestInList.py

--- 03List/10LargestInList.py
+++ 03List/10LargestInList.py
@@ -3,3 +3,18 @@ a = [10, 24, 76, 23, 12]
 # Max() will return the largest in 'a'
 largest = max(a)
 print(largest)
+
+a = [10, 24, 76, 23, 12]
+
+# Assuming first element is largest.
+largest = a[0]
+
+# Iterate through list and find largest
+for val in a:
+    if val > largest:
+      
+      	# If current element is greater than largest
+    	# update it
+        largest = val
+
+print(largest)
