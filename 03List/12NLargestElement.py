0b62b3239fe17da858059efe4bde3067ac82cb68
ShubhamKumarRaman <shubham620409@gmail.com>
2025-06-16 22:38:34 +0530

    Update 12NLargestElement.py

--- 03List/12NLargestElement.py
+++ 03List/12NLargestElement.py
@@ -26,3 +26,20 @@ N = 2
 
 # Calling the function
 Nmaxelements(list1, N)
+
+# Python program to find N largest
+# element from given list of integers
+
+l = [1000,298,3579,100,200,-45,900]
+n = 4
+
+l.sort()
+arr = []
+
+while n:
+    arr.append(l[-n])
+    n -= 1
+    
+print(arr)
+
+# This Code is contributed by Pratik Gupta
