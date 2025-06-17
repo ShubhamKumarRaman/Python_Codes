d5e45166d6c8eb0efd7a0139475756561fca6de2
ShubhamKumarRaman <shubham620409@gmail.com>
2025-06-17 23:11:48 +0530

    Update 12NLargestElement.py

--- 03List/12NLargestElement.py
+++ 03List/12NLargestElement.py
@@ -42,4 +42,19 @@ while n:
     
 print(arr)
 
-# This Code is contributed by Pratik Gupta
+import heapq
+
+
+def find_n_largest_elements(lst, N):
+    heap = lst
+    return heapq.nlargest(N, heap)
+
+
+# Test the function with given inputs
+lst = [4, 5, 1, 2, 9]
+N = 2
+print(find_n_largest_elements(lst, N))
+
+lst = [81, 52, 45, 10, 3, 2, 96]
+N = 3
+print(find_n_largest_elements(lst, N))
