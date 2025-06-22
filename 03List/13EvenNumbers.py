23c37ac930aa0da8126712450cb73f36bd58347c
ShubhamKumarRaman <shubham620409@gmail.com>
2025-06-22 22:54:53 +0530

    Update 13EvenNumbers.py

--- 03List/13EvenNumbers.py
+++ 03List/13EvenNumbers.py
@@ -13,3 +13,9 @@ a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 for val in a:
     if val % 2 == 0:
         print(val, end=" ")
+
+
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+res = [val for val in a if val & 1 == 0]
+print(res)
