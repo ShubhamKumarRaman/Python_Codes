fc11152366a8a01926faa55a88318a6a9caa7f8d
ShubhamKumarRaman <shubham620409@gmail.com>
2025-06-26 21:41:10 +0530

    Update 14OddNumbers.py

--- 03List/14OddNumbers.py
+++ 03List/14OddNumbers.py
@@ -9,3 +9,9 @@ a = [10, 15, 23, 42, 37, 51, 62, 5]
 res = [i for i in a if i % 2 != 0]
 
 print(res)
+
+a = [10, 15, 23, 42, 37, 51, 62, 5]
+
+res = filter(lambda x: x % 2 != 0, a)
+
+print(list(res))
