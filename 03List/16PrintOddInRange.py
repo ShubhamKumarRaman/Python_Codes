c2e74551a7f68c4e7afabb5d4ea548c136998d17
ShubhamKumarRaman <shubham620409@gmail.com>
2025-06-29 22:06:15 +0530

    Update 16PrintOddInRange.py

--- 03List/16PrintOddInRange.py
+++ 03List/16PrintOddInRange.py
@@ -4,3 +4,11 @@ for num in range(1, 11):
     # check if a number is odd
     if num % 2 != 0:
         print(num)
+
+start = 1
+end = 10
+
+# Use list comprehension to create a list of odd numbers
+# Iterate through the range (start to end + 1)
+odd_numbers = [num for num in range(start, end + 1) if num % 2 != 0]
+print(odd_numbers)
