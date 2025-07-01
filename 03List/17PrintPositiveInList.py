c52ec9475706558450c679868193d76644b5058b
ShubhamKumarRaman <shubham620409@gmail.com>
2025-07-01 23:26:13 +0530

    Update 17PrintPositiveInList.py

--- 03List/17PrintPositiveInList.py
+++ 03List/17PrintPositiveInList.py
@@ -10,3 +10,11 @@ a = [-10, 15, 0, 20, -5, 30, -2]
 # List comprehension to filter positive numbers
 res = [i for i in a if i > 0]
 print(res)
+
+a = [-10, 15, 0, 20, -5, 30, -2]
+
+# Using filter() to get positive numbers
+res = filter(lambda x: x > 0, a)
+
+# Convert filter object to list and print
+print(list(res))
