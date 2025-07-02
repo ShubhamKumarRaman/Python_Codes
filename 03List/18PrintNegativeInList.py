01be098e89b2d4015c1bc057e35ed405b10c0415
ShubhamKumarRaman <shubham620409@gmail.com>
2025-07-02 23:24:17 +0530

    Update 18PrintNegativeInList.py

--- 03List/18PrintNegativeInList.py
+++ 03List/18PrintNegativeInList.py
@@ -4,3 +4,10 @@ a = [5, -3, 7, -1, 2, -9, 4]
 for num in a:
     if num < 0:
         print(num)
+
+a = [5, -3, 7, -1, 2, -9, 4]
+
+# Using list comprehension to filter negative numbers
+n = [num for num in a if num < 0]
+
+print(n)
