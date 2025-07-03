35164936b46add5ec53a759831c4f5789e59795d
ShubhamKumarRaman <shubham620409@gmail.com>
2025-07-03 23:42:29 +0530

    Update 18PrintNegativeInList.py

--- 03List/18PrintNegativeInList.py
+++ 03List/18PrintNegativeInList.py
@@ -18,3 +18,13 @@ a = [5, -3, 7, -1, 2, -9, 4]
 # Using filter function to find negative numbers
 n = list(filter(lambda x: x < 0, a))
 print(n)
+
+# Using map function
+a = [5, -3, 7, -1, 2, -9, 4]
+
+# Using map to filter negative numbers
+n = list(map(lambda x: x if x < 0 else None, a))
+
+# Removing None values and printing negative numbers
+n = [num for num in n if num is not None]
+print(n)
