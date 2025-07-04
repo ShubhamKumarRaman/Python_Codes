43b568523a9c2a9044223a4514cd349f8a1054ba
ShubhamKumarRaman <shubham620409@gmail.com>
2025-07-04 23:15:42 +0530

    Create J19PrintPositiveInRange.py

---
+++ 03List/J19PrintPositiveInRange.py
@@ -0,0 +1,10 @@
+start = -5
+end = 3
+
+# Loop through range and print positive numbers
+for val in range(start, end + 1):
+  
+    # checks whether current number is positive
+    if val > 0:
+        print(val)
+
