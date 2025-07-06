4df707a59fe4ed28e969a8e4b7b4c6a3e0a6514a
ShubhamKumarRaman <shubham620409@gmail.com>
2025-07-06 22:16:56 +0530

    Update J19PrintPositiveInRange.py

--- 03List/J19PrintPositiveInRange.py
+++ 03List/J19PrintPositiveInRange.py
@@ -8,3 +8,9 @@ for val in range(start, end + 1):
     if val > 0:
         print(val)
 
+start = -5
+end = 3
+
+# Use list comprehension to filter positive numbers and print them
+res = [val for val in range(start, end + 1) if val > 0]
+print(res)
