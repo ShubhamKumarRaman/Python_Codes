d76bbecd63c86ec74a7d1407893d1a13dce70600
ShubhamKumarRaman <shubham620409@gmail.com>
2025-07-09 21:44:02 +0530

    Update 20PrintNegativeInRange.py

--- 03List/20PrintNegativeInRange.py
+++ 03List/20PrintNegativeInRange.py
@@ -2,3 +2,9 @@ start, end = -5, 0
 for i in range(start, end + 1):  
     if i < 0:  # Check if the number is negative
         print(i)
+
+start, end = -10, 0
+# List comprehension to generate a list of negative numbers within the range
+n = [i for i in range(start, end + 1) if i < 0]  
+print(n)
+
