1e9978e6defd52b91fefe870c9983e9fc9e7d944
ShubhamKumarRaman <shubham620409@gmail.com>
2025-07-09 21:44:28 +0530

    Update 20PrintNegativeInRange.py

--- 03List/20PrintNegativeInRange.py
+++ 03List/20PrintNegativeInRange.py
@@ -8,3 +8,9 @@ start, end = -10, 0
 n = [i for i in range(start, end + 1) if i < 0]  
 print(n)
 
+start, end = -10, 0 
+
+# Use filter() to keep only  negative numbers from  range and convert result to a list
+n = list(filter(lambda x: x < 0, range(start, end + 1)))  
+
+print(n)
