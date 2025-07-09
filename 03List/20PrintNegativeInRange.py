96d2ddc971b56de27791988d1fe396d0144172c9
ShubhamKumarRaman <shubham620409@gmail.com>
2025-07-09 21:44:42 +0530

    Update 20PrintNegativeInRange.py

--- 03List/20PrintNegativeInRange.py
+++ 03List/20PrintNegativeInRange.py
@@ -14,3 +14,10 @@ start, end = -10, 0
 n = list(filter(lambda x: x < 0, range(start, end + 1)))  
 
 print(n)
+
+start, end = -10, 0  
+n = list(map(lambda x: x if x < 0 else None, range(start, end + 1)))  
+
+# Use list comprehension to remove None values (non-negative numbers)
+n = [num for num in n if num is not None]  
+print(n)
