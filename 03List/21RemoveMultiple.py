d673f4a24891a68a0d2c99cd4bce6ec0f2773bf2
ShubhamKumarRaman <shubham620409@gmail.com>
2025-07-11 22:26:47 +0530

    Update 21RemoveMultiple.py

--- 03List/21RemoveMultiple.py
+++ 03List/21RemoveMultiple.py
@@ -11,3 +11,14 @@ for val in a:
         res.append(val)
 
 print(res)
+
+
+a = [10, 20, 30, 40, 50, 60, 70]
+
+# Elements to remove
+remove = [20, 40, 60]
+
+# Remove elements using list comprehension
+a = [x for x in a if x not in remove]
+
+print(a)
