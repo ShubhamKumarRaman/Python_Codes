57a9349f9e641da064ab3eb92cbb098f3eaad36a
ShubhamKumarRaman <shubham620409@gmail.com>
2025-07-13 22:40:50 +0530

    Update 22RemoveEmptyList.py

--- 03List/22RemoveEmptyList.py
+++ 03List/22RemoveEmptyList.py
@@ -15,3 +15,14 @@ res = [b for b in a if b]
 
 print(res)
 
+a = [[1, 2], [], [3, 4], [], [5]]
+
+res = list(filter(lambda b: b, a))
+
+# By using None as the first parameter in filter,
+# it removes any falsy values (like empty list)
+
+# res = list(filter(None, a)) # This will also work
+
+print(res)
+
